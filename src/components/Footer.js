236401e72ae82661a94ec557ec190746141eda10
PiotrGasiorek <piotrjgasiorek@gmail.com>
2020-04-30 00:23:23 +0200

    Created new component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer container">
+        <p className="footer__paragraph paragraph">
+            Created by Piotr Gąsiorek
+        </p>
+    </footer>
+  );
+}
+
+export default Footer;
